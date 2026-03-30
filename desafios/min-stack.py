c99cfa623be8ccedddd1c3e1f3a085ce872be027
cauefer <cauefernandescaetano@gmail.com>
2026-03-29 21:13:07 -0300

    Adiciona o desafio "min-stack"

---
+++ desafios/min-stack.py
@@ -0,0 +1,30 @@
+# Desafio Min Stack
+# Link: https://leetcode.com/problems/min-stack/
+# Estrutura de dados: Pilha
+
+# Integrante: Walter Theodoro
+
+# Problema
+# Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+# Implement the MinStack class with push, pop, top and getMin operations.
+
+# Solução
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val):
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self):
+        if self.stack.pop() == self.min_stack[-1]:
+            self.min_stack.pop()
+
+    def top(self):
+        return self.stack[-1]
+
+    def getMin(self):
+        return self.min_stack[-1]
